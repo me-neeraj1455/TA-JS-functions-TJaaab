9558bde16994f4d6440342bff891b9fe31453584
Neeraj-kumar1411 <welcometoneeraj@gmail.com>
2021-12-26 17:03:55 +0530

    function

--- block-BJaabl/code/index.js
+++ block-BJaabl/code/index.js
@@ -1,6 +1,10 @@
 /*
 1. Create a function named `sayHello` that that accepts a parameter `name` and alert `Hello [name]!`.
 */
+// function sayHello(name) {
+//   alert(name);
+// }
+// sayHello(`Ankit`);
 
 /*
 2. Create a function named `getFullName` that accepts two parameter `firstName` and `lastName` and returns
@@ -11,6 +15,10 @@ Example:
   getFullName("Nelson", "Mandela"); // "Nelson Mandela"
 */
 
+// function getFullName(firstName, lastName) {
+//   return `${firstName}  ${lastName}`;
+// }
+// getFullName("John", "Snow");
 /*
 3. Create a function named `addTwoNumbers` that accepts two numbers i.e `firstNum` and `secondNum` and returns
 the sum of both numbers. If user passes any data other then numbers alert a message saying `Enter Valid Input`
@@ -20,6 +28,15 @@ addTwoNumbers(10, 22); // 32
 addTwoNumbers(20, 32); // 32
 addTwoNumbers(10, "100"); // Alert Enter Valid Input
 */
+// function addTwoNumbers(firstNum, secondNum) {
+//   if (firstNum || secondNum !==typeof Number) {
+//     alert(`Enter a valid Input`);
+//   } else {
+//     sum = firstNum + secondNum;
+//     return sum;
+//   }
+// }
+// addTwoNumbers(5, 5);
 
 /*
 4. Create a function named `calc` which accepts three parameter `numA`, `numB` and `operation`. Operation
@@ -33,7 +50,6 @@ calc(20, 10, 'sub'); // 10
 calc(20, 10, 'mul'); // 200
 */
 
-
 /*
 5. Create a function named `isLeapYear` that accepts a number data type and return `true` or `false` based
 on if the year id leap year or not.
@@ -41,7 +57,6 @@ on if the year id leap year or not.
 isLeapYear(2000); // true
 isLeapYear(2001); // false
 */
-
 
 /*
 6. Create a function named `getFactorial` that accepts a number and return the factorial of the number.
